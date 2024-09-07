84d666dd8b59f840d29895d82074909bd6b8d742
soo <kimmngsu@gamil.com>
2024-09-07 20:01:34 +0900

    Create Project

---
+++ spring-security-learn-practice/src/main/java/com/example/spring_security_learn_practice/SpringSecurityLearnPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_security_learn_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityLearnPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityLearnPracticeApplication.class, args);
+	}
+
+}
---
+++ spring-security-learn-practice/src/test/java/com/example/spring_security_learn_practice/SpringSecurityLearnPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_security_learn_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityLearnPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
