b532eb051c0a0aa9d1fe8a505b0dcfa35c3d2d97
soo <kimmngsu@gamil.com>
2024-09-16 17:53:40 +0900

    CSRF 사용하는 리소스 생성 (csrf 속성 조회하는 로직 만들기)

--- spring-security-learn-practice/src/main/java/com/example/spring_security_learn_practice/resources/SpringSecurityPlayResource.java
+++ spring-security-learn-practice/src/main/java/com/example/spring_security_learn_practice/resources/SpringSecurityPlayResource.java
@@ -1,12 +1,15 @@
 package com.example.spring_security_learn_practice.resources;
 
+import org.springframework.security.web.csrf.*;
 import org.springframework.web.bind.annotation.*;
+
+import jakarta.servlet.http.*;
 
 @RestController
 public class SpringSecurityPlayResource {
 
-	@GetMapping("/hello-world")
-	public String helloWorld() {
-		return "Hello World v1";
+	@GetMapping("/csrf-token")
+	public CsrfToken retrieveCsrfToke(HttpServletRequest request) {
+		return (CsrfToken) request.getAttribute("_csrf");
 	}
 }
