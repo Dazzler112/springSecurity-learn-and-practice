0952f96c1137c41d5bb418cb56d349c71c8e527a
soo <kimmngsu@gamil.com>
2024-09-20 17:24:36 +0900

    InMemory 방식으로 사용자 자격 증명 저장하기

--- spring-security-learn-practice/src/main/java/com/example/spring_security_learn_practice/basic/BasicAuthSecurityConfiguration.java
+++ spring-security-learn-practice/src/main/java/com/example/spring_security_learn_practice/basic/BasicAuthSecurityConfiguration.java
@@ -5,6 +5,8 @@ import static org.springframework.security.config.Customizer.*;
 import org.springframework.context.annotation.*;
 import org.springframework.security.config.annotation.web.builders.*;
 import org.springframework.security.config.http.*;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.security.provisioning.*;
 import org.springframework.security.web.*;
 
 @Configuration
@@ -29,5 +31,22 @@ public class BasicAuthSecurityConfiguration {
 		http.csrf().disable();
 		
 		return http.build();
+	}
+	
+	//🔵 InMemory 방식
+	@Bean
+	public UserDetailsService userDetailsService() {
+		
+		var user = User.withUsername("player") //사용자 이름은 player로 설정
+			.password("{noop}dummy")//아직 인코딩하지 않을거여서 noop를 사용하고 패스워드를 dummy로 하겠다
+			.roles("USER") //역할은 USER
+			.build();
+		
+		var admin = User.withUsername("admin") //사용자 이름은 admin로 설정
+				.password("{noop}dummy")//패스워드는 위와 동일하게
+				.roles("ADMIN") //역할은 ADMIN
+				.build();
+			
+		return new InMemoryUserDetailsManager(user, admin); //이곳에 user,admin을 입력
 	}
 }
--- spring-security-learn-practice/src/main/java/com/example/spring_security_learn_practice/resources/TodoResource.java
+++ spring-security-learn-practice/src/main/java/com/example/spring_security_learn_practice/resources/TodoResource.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TodoResource {
-
+	
 	private Logger logger = LoggerFactory.getLogger(getClass());
 	
 	private static final List<Todo> TODOS_LIST = 
@@ -17,18 +17,19 @@ public class TodoResource {
 	@GetMapping("/todos")
 	public List<Todo> retrieveAllTodos() {
 		return TODOS_LIST;
-	} //=>200을 반환
-	
+	}
+
 	@GetMapping("/users/{username}/todos")
 	public Todo retrieveTodosForSpecificUser(@PathVariable String username) {
 		return TODOS_LIST.get(0);
-	} //=> 200을 반환
-	
+	}
+
 	@PostMapping("/users/{username}/todos")
-	public void createTodosForSpecificUser(@PathVariable String username,
-										@RequestBody Todo todo) {
+	public void createTodoForSpecificUser(@PathVariable String username
+			, @RequestBody Todo todo) {
 		logger.info("Create {} for {}", todo, username);
-	} //=> 401을 반환 why? 읽기요청은 허용했지만 post는 왜? CSRF 토큰 필요!
+	}
+
 }
 
 record Todo (String username, String description) {}
